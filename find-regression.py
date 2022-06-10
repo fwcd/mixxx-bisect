f62b8bf7b9942de7d8ce98b1abdf2d9d54f8a390
fwcd <fwcdmail@gmail.com>
2022-06-10 04:14:50 +0200

    Sort commits using git

--- find-regression.py
+++ find-regression.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python3
 
 import requests
+import subprocess
 
 from bs4 import BeautifulSoup
+from pathlib import Path
 
 SNAPSHOTS_URL = 'https://downloads.mixxx.org/snapshots/main/'
 SUFFIX = '.dmg'
+BASE_DIR = Path(__file__).resolve().parent
+MIXXX_DIR = BASE_DIR / 'mixxx.git'
 
 def get_soup(url: str) -> BeautifulSoup:
     headers = {'User-Agent': 'MixxxRegressionFinder/0.0.1'}
     response = requests.get(url, headers=headers)
     return BeautifulSoup(response.content, 'html.parser')
 
-def parse_commit(link: str):
+def parse_commit(link: str) -> str:
     name = link.split('/')[-1].removesuffix(SUFFIX).removesuffix('-macosintel')
     return name.split('-')[-1]
 
-def main():
+def fetch_snapshots() -> dict[str, str]:
     snapshot_soup = get_soup(SNAPSHOTS_URL)
     links = [a.get('href') for a in snapshot_soup.select('a')]
-    snapshots = [(parse_commit(link), link) for link in links if link.endswith(SUFFIX)]
-    print('\n'.join(commit + ', ' + link for commit, link in snapshots))
+    return {parse_commit(link): link) for link in links if link.endswith(SUFFIX)}
+
+def clone_mixxx():
+    if not MIXXX_DIR.exists():
+        print('==> Cloning Mixxx...')
+        subprocess.run(['git', 'clone', '--bare', 'https://github.com/mixxxdj/mixxx.git', str(MIXXX_DIR)], cwd=BASE_DIR)
+
+def sort_commits(commits: list[str]) -> list[str]:
+    raw = subprocess.run(['git', 'rev-list', '--no-walk'] + commits, cwd=MIXXX_DIR, capture_output=True, encoding='utf8').stdout
+    return raw.splitlines()
+
+def main():
+    clone_mixxx()
 
 if __name__ == '__main__':
     main()
